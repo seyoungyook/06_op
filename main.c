3120bcc23345126ee492fc7f0cf1f2fc5b3f9abf
seyoungyook <xqd1219@naver.com>
2019-10-10 15:54:22 +0900

    practice2

--- main.c
+++ main.c
@@ -3,20 +3,32 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star() {
 	
-	int i;
-	
-	for( i = 0; i < 10; i++)
-	printf("*");
+int sumTwo( int a, int b)
+{
+	//int output;
+	//output = a + b;
+	return a+b;//output
 }
 
-int main(void)
-
+int square ( int n )
 {
-	print_star();
-	print_star();
-	print_star();
+	return (n*n);
+}
+
+int get_max( int a, int b )
+{
+	if ( a>b ) return (a);
+	else return (b);
+}
+
+int main(int argc, char *argv[]){
+	
+	sumTwo;
+	square;
+	get_max;
 	
 	return 0;
 }
+
+
